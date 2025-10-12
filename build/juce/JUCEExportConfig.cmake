78670c7473dd903a36f92bdcb91006e639477696
Kris <kristofferok@icloud.com>
2025-10-12 22:01:12 +0200

    build

--- build/juce/JUCEExportConfig.cmake
+++ build/juce/JUCEExportConfig.cmake
@@ -54,7 +54,7 @@ endif()
 
 check_required_components("JUCE")
 
-set(JUCE_MODULES_DIR "/Users/kris/Dev/ProTune/JUCE/modules" CACHE INTERNAL
+set(JUCE_MODULES_DIR "${PACKAGE_PREFIX_DIR}/JUCE/modules" CACHE INTERNAL
     "The path to JUCE modules")
 
 include("/Users/kris/Dev/ProTune/JUCE/extras/Build/CMake/JUCEModuleSupport.cmake")
@@ -113,7 +113,7 @@ unset(_targets_defined)
 unset(_targets_expected)
 
 foreach(_juce_module IN LISTS _juce_modules)
-    juce_add_module("/Users/kris/Dev/ProTune/JUCE/modules/${_juce_module}" ALIAS_NAMESPACE juce)
+    juce_add_module("${PACKAGE_PREFIX_DIR}/JUCE/modules/${_juce_module}" ALIAS_NAMESPACE juce)
 endforeach()
 
 unset(_juce_modules)
